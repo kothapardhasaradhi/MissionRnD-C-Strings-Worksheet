2a0fee1076bff6700f3a78076a2aa6037ca8694b
kothapardhasaradhi <kothapardhasaradhi@missionrnd.com>
2016-03-18 21:47:48 +0530

    Submit Commit

--- src/commonWords.cpp
+++ src/commonWords.cpp
@@ -19,5 +19,72 @@ NOTES: If there are no common words return NULL.
 
 char ** commonWords(char *str1, char *str2) 
 {
-	return NULL;
+	if (str1&&str2)
+	{
+		int i, j = 0, k, t = 0, p, flag = 0, a = 0, b = 0;
+		char *s, **ans;
+		ans = (char **)malloc(sizeof(char)*SIZE);
+		s = (char*)malloc(sizeof(char)* 100);
+		for (i = 0; str1[j] != '\0'; i++)
+		{
+			t = 0;
+			for (; str1[j] != ' '&&str1[j] != '\0'; j++)
+			{
+				s = (char*)realloc(s, t + 1);
+				s[t++] = str1[j];
+			}
+			s[t] = '\0';
+			for (j = j + 1; str1[j] == ' '; j++)
+			{
+
+			}
+			for (k = 0, p = 0; str2[k] != '\0'; k++)
+			{
+				ans[a] = (char*)malloc(sizeof(char)*SIZE);
+				if (s[p] == str2[k])
+				{
+					p++; k++;
+					for (; s[p] != '\0'; p++, k++)
+					{
+						if (s[p] == str2[k])
+						{
+
+							flag = 1;
+						}
+						else
+						{
+							flag = 0;
+							break;
+						}
+
+					}
+					if (flag == 1)
+					{
+						for (b = 0; s[b] != '\0'; b++)
+						{
+							ans[a][b] = s[b];
+						}
+						//ans[a] = s;
+						ans[a][b] = '\0';
+						a++;
+
+						//printf("%s\n",ans[a-1]);     //strng found;
+					}
+				}
+				for (k = k; str2[k] != ' '&&str2[k] != '\0'; k++)
+				{
+
+				}
+
+			}
+
+		}
+		if (a == 0)
+			return NULL;
+		ans[a] = '\0';
+		return ans;
+	}
+	else
+		return NULL;
+
 }
